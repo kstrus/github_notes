a794a82988651ed573372068449bf2c753eddb1f
katarzynastrus <katarzyna.strus@grandparade.co.uk>
2016-10-07 16:04:02 +0200

    Add router and Profile component

---
+++ app/components/Profile.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var Router = require('rect-router');
+
+var Profile = React.createClass({
+    getInitialState: function() {
+
+    },
+
+    render: function() {
+        return (
+            <div className = "row">
+                <div className = "col-md-4">
+                    User Profile Component
+                </div>
+                <div className = "col-md-4">
+                    Repos Component
+                </div>
+                <div className = "col-md-4">
+                    Notes Component
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = Profile;
